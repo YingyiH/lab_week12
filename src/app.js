a09023ec4a047f8c7fdf8f25e28175d368f22404
Yingyi He <yhe77@my.bcit.ca>
2022-03-25 12:28:45 -0700

    Add javascript code to calculate student average grades with functions in app.js

--- src/app.js
+++ src/app.js
@@ -4,3 +4,43 @@ function sele_elem(element) {
     return selection_single
 }
 console.log(sele_elem('nav'))
+
+//part 3
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+  
+function calc_scholarship(student_array) {
+    const studentsScholarship = []
+    
+    for (const member in student_array) {
+
+        let total_grade = 0
+        let average_grade = 0
+
+        function calc_avg(student_array){
+
+            for(const student_grade of student_array[member].grades){  
+                total_grade += student_grade 
+            }
+            average_grade = total_grade/4
+        }
+
+        calc_avg(students)
+        
+        if (average_grade > 80){
+            const student_name = student_array[member].name
+            studentsScholarship.push(
+                {
+                    name: student_name,
+                    gradeAvg: average_grade
+                }
+            )
+        } 
+    }
+    console.log(studentsScholarship)
+}
+calc_scholarship(students)
